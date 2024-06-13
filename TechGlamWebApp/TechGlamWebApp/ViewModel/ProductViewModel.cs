57cb0eb0aa3904d5ed1b4f4d1dd8fa1629576473
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-13 22:53:23 +0200

    create ProductViewModel.cs

---
+++ TechGlamWebApp/TechGlamWebApp/ViewModel/ProductViewModel.cs
@@ -0,0 +1,19 @@
+using WebApp.enumeration;
+
+namespace WebApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Guid ProductID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public WebAppEnum.SizeRingsBracelets? SizeRingsBracelets { get; set; }
+        public WebAppEnum.SizeWatches? SizeWatches { get; set; } // Nullable for watches only
+        public string MetalType { get; set; }
+    }
+}
