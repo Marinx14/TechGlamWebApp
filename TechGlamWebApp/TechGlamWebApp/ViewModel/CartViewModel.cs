2f83217b3a5ae7c32e061107b0986bea4a467ec7
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-13 22:48:17 +0200

    creazione view model cart

---
+++ TechGlamWebApp/TechGlamWebApp/ViewModel/CartViewModel.cs
@@ -0,0 +1,10 @@
+namespace WebApp.ViewModels
+
+{
+    public class CartViewModel
+    {
+        public Guid CartID { get; set; }
+        public List<ProductViewModel> Products { get; set; } = new List<ProductViewModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
