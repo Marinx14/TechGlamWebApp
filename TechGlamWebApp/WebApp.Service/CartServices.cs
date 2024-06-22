c38d28bdcc6a269632fdb9d65b8e0408fd6e33d6
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-22 15:33:22 +0200

    Correzione CartServices.cs

---
+++ TechGlamWebApp/WebApp.Service/CartServices.cs
@@ -0,0 +1,21 @@
+using WebApp.Data;
+
+namespace WebApp.Service;
+
+public class CartServices
+{
+    public CartServices(AppDbContext dbContext)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string?> GetCartAsync(string? userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task GetProductByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
